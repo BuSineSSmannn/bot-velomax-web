d10037a2f03f1aefb331bc30cd9acab548d146af
Jason Dev <telegram.org@mail.ru>
2023-10-11 17:09:30 +0500

    fox

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <AnketaForm></AnketaForm>
+</template>
+
+<script>
+import AnketaForm from "@/components/AnketaForm.vue";
+
+export default {
+  name: 'App',
+  components: {
+    AnketaForm,
+  }
+}
+</script>
+
+<style>
+
+
+</style>
